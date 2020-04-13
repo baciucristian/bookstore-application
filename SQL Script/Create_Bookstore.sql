e4ca21d5fd642c90998fd2fd9d1662b1d48c009a
Cristian Baciu <icristianbaciu@gmail.com>
2020-04-13 22:01:41 +0300

    Add data to tables

--- SQL Script/Create_Bookstore.sql
+++ SQL Script/Create_Bookstore.sql
@@ -16,7 +16,7 @@ use Library
 go
 create table Furnizori
 (
-	idFurnizor int primary key -- Cheie primară ce identifică furnizorul
+	idFurnizor int IDENTITY(1,1) primary key -- Cheie primară ce identifică furnizorul
 	,denumireFurnizor nvarchar(100) UNIQUE -- Denumirea furnizorului
     ,tipFurnizor nvarchar(100) -- Tipul furnizorului (editura, depozit, persoana particulara)
     ,adresaFurnizor nvarchar(100) -- Adresa furnizorului
@@ -24,20 +24,19 @@ create table Furnizori
 )
 create table Autori
 (
-    idAutor int primary key -- Cheie primară ce identifică autorul
+    idAutor int IDENTITY(1,1) primary key -- Cheie primară ce identifică autorul
     ,numeAutor nvarchar(100) -- Numele și prenumele autorului
     ,dataAutor nvarchar(100) -- Data nașterii autorului
     ,genAutor nvarchar(1) -- Genul (M sau F)
 )
 create table Carti
 ( 
-	idCarte int primary key -- Cheie primară ce identifică cartea
+	idCarte int IDENTITY(1,1) primary key -- Cheie primară ce identifică cartea
 	,idFurnizor int foreign key (idFurnizor) references Furnizori -- Face legatura cu tabelul Furnizori
 	,denumireCarte nvarchar(100) UNIQUE -- Denumirea cartii
     ,idAutor int foreign key (idAutor) references Autori -- Face legatura cu tabelul Autori
     ,anCarte nvarchar(100) -- Anul editarii cartii
     ,pretCarte int -- Pretul cartii
-    
 )
 create table Users
 ( 
@@ -47,18 +46,26 @@ create table Users
 )
 GO
 --Inserarea datelor
-INSERT INTO Furnizori (idFurnizor, denumireFurnizor, tipFurnizor, adresaFurnizor, telefonFurnizor) VALUES 
-	(1, 'Editura Arc', 'Editura', 'str. M. Eminescu 23', '067123376')
-    ,(2, 'Editura Cartier', 'Editura', 'str. Sarmisezetusa 87/B', '067233479')
-    ,(3, 'Jian Catalin', 'Persoana', 'str. Bucuriei 1', '043829874')
+INSERT INTO Furnizori (denumireFurnizor, tipFurnizor, adresaFurnizor, telefonFurnizor) VALUES 
+	('Editura Arc', 'Editura', 'str. M. Eminescu 23', '067123376')
+    ,('Editura Cartier', 'Editura', 'str. Sarmisezetusa 87/B', '067233479')
+    ,('Jian Catalin', 'Persoana', 'str. Bucuriei 1', '063829874')
+	,(N'Negară Maxim', 'Persoana', 'str. Grigore Vieru 54', '067124492')
+	,('Editura Litera', 'Editura', 'str. M. Eminescu 90', '067298349')
 GO
-INSERT INTO Autori (idAutor, numeAutor, dataAutor, genAutor) VALUES
-    (1, 'Ion Creangă', '01.03.1837', 'M')
-    ,(2, 'Mihai Eminescu', '15.01.1850', 'M')
+INSERT INTO Autori (numeAutor, dataAutor, genAutor) VALUES
+    (N'Ion Creangă', '01.03.1837', 'M')
+    ,('Mihai Eminescu', '15.01.1850', 'M')
+	,('Miron Costin', '30.03.1633', 'M')
+	,('Veronica Micle', '22.04.1850', 'F')
+	,('Ion Pillat', '31.03.1891', 'M')
 GO
-INSERT INTO Carti (idCarte, idFurnizor, denumireCarte, idAutor, anCarte, pretCarte) VALUES
-    (1, 3, 'Amintiri din copilărie', 1, '1892', 52)
-    ,(2, 1, 'Luceafărul', 2,'1883', 86)
+INSERT INTO Carti (idFurnizor, denumireCarte, idAutor, anCarte, pretCarte) VALUES
+    (3, N'Amintiri din copilărie', 1, '1892', 52)
+    ,(1, N'Luceafărul', 2,'1883', 86)
+	,(4, N'Letopisețul Țării Moldovei', 3, 1661, 138)
+	,(5, N'Viața Lumii', 3, 1672, 102)
+	,(2, N'Plimbarea de mai în Iaşi', 4, 1872, 83)
 GO
 INSERT INTO Users (username, password) VALUES 
     ('admin', 'admin')
