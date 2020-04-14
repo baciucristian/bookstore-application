4ac4bd5524f5ba4196c44015f8b283dac2b45b8c
Cristian Baciu <icristianbaciu@gmail.com>
2020-04-14 17:10:08 +0300

    Add furnizori form

---
+++ furnizoriForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Bookstore
+{
+    public partial class furnizoriForm : Form
+    {
+        public furnizoriForm()
+        {
+            InitializeComponent();
+        }
+
+        private void furnizoriForm_Load(object sender, EventArgs e)
+        {
+            // Database connection
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\School\Anul III\Practica Anul III\bookstore-application\Bookstore.mdf;Integrated Security=True");
+
+            string query = "SELECT denumireFurnizor, tipFurnizor, adresaFurnizor, telefonFurnizor FROM Furnizori";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
