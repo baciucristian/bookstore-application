24f6ef79ec5862a43946ddbe8adb30f141cf7230
Cristian Baciu <icristianbaciu@gmail.com>
2020-04-14 18:16:28 +0300

    Add functionality to Tabele tab

--- showForm.Designer.cs
+++ showForm.Designer.cs
@@ -28,30 +28,291 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(showForm));
+            this.panelSideMenu = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnMaximal = new System.Windows.Forms.Button();
+            this.panelTabele = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnTabele = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelChildForm = new System.Windows.Forms.Panel();
+            this.panelSideMenu.SuspendLayout();
+            this.panelTabele.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // bunifuElipse1
+            // panelSideMenu
             // 
-            this.bunifuElipse1.ElipseRadius = 20;
-            this.bunifuElipse1.TargetControl = this;
+            this.panelSideMenu.AutoScroll = true;
+            this.panelSideMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(52)))), ((int)(((byte)(71)))));
+            this.panelSideMenu.Controls.Add(this.button7);
+            this.panelSideMenu.Controls.Add(this.button6);
+            this.panelSideMenu.Controls.Add(this.button1);
+            this.panelSideMenu.Controls.Add(this.btnMaximal);
+            this.panelSideMenu.Controls.Add(this.panelTabele);
+            this.panelSideMenu.Controls.Add(this.btnTabele);
+            this.panelSideMenu.Controls.Add(this.panel1);
+            this.panelSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSideMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelSideMenu.Name = "panelSideMenu";
+            this.panelSideMenu.Size = new System.Drawing.Size(250, 581);
+            this.panelSideMenu.TabIndex = 12;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(0, 468);
+            this.button7.Name = "button7";
+            this.button7.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button7.Size = new System.Drawing.Size(250, 50);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "Cărțile în română";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(0, 418);
+            this.button6.Name = "button6";
+            this.button6.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button6.Size = new System.Drawing.Size(250, 50);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Cărțile autorului";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(0, 368);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button1.Size = new System.Drawing.Size(250, 50);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Preț total";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnMaximal
+            // 
+            this.btnMaximal.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMaximal.FlatAppearance.BorderSize = 0;
+            this.btnMaximal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaximal.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.btnMaximal.ForeColor = System.Drawing.Color.White;
+            this.btnMaximal.Location = new System.Drawing.Point(0, 318);
+            this.btnMaximal.Name = "btnMaximal";
+            this.btnMaximal.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnMaximal.Size = new System.Drawing.Size(250, 50);
+            this.btnMaximal.TabIndex = 15;
+            this.btnMaximal.Text = "Furn. cu cărți maximale";
+            this.btnMaximal.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMaximal.UseVisualStyleBackColor = true;
+            this.btnMaximal.Click += new System.EventHandler(this.btnMaximal_Click);
+            // 
+            // panelTabele
+            // 
+            this.panelTabele.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(52)))), ((int)(((byte)(71)))));
+            this.panelTabele.Controls.Add(this.button5);
+            this.panelTabele.Controls.Add(this.button3);
+            this.panelTabele.Controls.Add(this.button2);
+            this.panelTabele.Controls.Add(this.button4);
+            this.panelTabele.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTabele.Location = new System.Drawing.Point(0, 150);
+            this.panelTabele.Name = "panelTabele";
+            this.panelTabele.Size = new System.Drawing.Size(250, 168);
+            this.panelTabele.TabIndex = 14;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(0, 120);
+            this.button5.Name = "button5";
+            this.button5.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button5.Size = new System.Drawing.Size(250, 40);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Cărți Limbi";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(0, 80);
+            this.button3.Name = "button3";
+            this.button3.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button3.Size = new System.Drawing.Size(250, 40);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Autori";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(0, 40);
+            this.button2.Name = "button2";
+            this.button2.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button2.Size = new System.Drawing.Size(250, 40);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Furnizori";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.button4.Size = new System.Drawing.Size(250, 40);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Cărți";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnTabele
+            // 
+            this.btnTabele.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTabele.FlatAppearance.BorderSize = 0;
+            this.btnTabele.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTabele.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.btnTabele.ForeColor = System.Drawing.Color.White;
+            this.btnTabele.Location = new System.Drawing.Point(0, 100);
+            this.btnTabele.Name = "btnTabele";
+            this.btnTabele.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.btnTabele.Size = new System.Drawing.Size(250, 50);
+            this.btnTabele.TabIndex = 13;
+            this.btnTabele.Text = "Tabele";
+            this.btnTabele.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTabele.UseVisualStyleBackColor = true;
+            this.btnTabele.Click += new System.EventHandler(this.btnTabele_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 100);
+            this.panel1.TabIndex = 10;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(55, 7);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(131, 87);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(29)))), ((int)(((byte)(38)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(250, 481);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(797, 100);
+            this.panel2.TabIndex = 13;
+            // 
+            // panelChildForm
+            // 
+            this.panelChildForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(45)))), ((int)(((byte)(59)))));
+            this.panelChildForm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelChildForm.Location = new System.Drawing.Point(250, 0);
+            this.panelChildForm.Name = "panelChildForm";
+            this.panelChildForm.Size = new System.Drawing.Size(797, 481);
+            this.panelChildForm.TabIndex = 14;
             // 
             // showForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(150)))), ((int)(((byte)(243)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1047, 581);
+            this.Controls.Add(this.panelChildForm);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelSideMenu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(972, 628);
             this.Name = "showForm";
-            this.Text = "showForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Afișare";
+            this.Load += new System.EventHandler(this.showForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.showForm_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.showForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.showForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.showForm_MouseUp);
+            this.panelSideMenu.ResumeLayout(false);
+            this.panelTabele.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Panel panelSideMenu;
+        private System.Windows.Forms.Button btnTabele;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelTabele;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnMaximal;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelChildForm;
     }
 }
--- showForm.cs
+++ showForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Bookstore
 {
@@ -16,5 +17,140 @@ namespace Bookstore
         {
             InitializeComponent();
         }
+
+        // Database connection
+        // SqlConnection sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\School\Anul III\Practica Anul III\bookstore - application\Bookstore.mdf;Integrated Security=True");
+
+        #region Design
+
+        private Form activeForm = null;
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelChildForm.Controls.Add(childForm);
+            panelChildForm.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void openChildFormPanel2(Form childForm)
+        {
+            if (activeForm != null)
+                activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panel2.Controls.Add(childForm);
+            panel2.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        // Submenu design
+        private void customizeDesign()
+        {
+            panelTabele.Visible = false;
+        }
+
+        private void hideSubMenu()
+        {
+            if (panelTabele.Visible == true) { }
+                panelTabele.Visible = false;
+        }
+
+        private void showSubMenu(Panel SubMenu)
+        {
+            if (SubMenu.Visible == false)
+            {
+                hideSubMenu();
+                SubMenu.Visible = true;
+            }
+            else
+                SubMenu.Visible = false;
+        }
+        #endregion
+
+        private void showForm_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void showForm_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void showForm_MouseUp(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void showForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showForm_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void tabControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Hide();
+                menuForm menuForm = new menuForm();
+                menuForm.Show();
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMaximal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTabele_Click(object sender, EventArgs e)
+        {
+            showSubMenu(panelTabele);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openChildForm(new furnizoriForm());
+            hideSubMenu();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openChildForm(new autoriForm());
+            hideSubMenu();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openChildForm(new cartiForm());
+
+            hideSubMenu();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            openChildFormPanel2(new cartiLimbaFormSearch());
+            hideSubMenu();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
