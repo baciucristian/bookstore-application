841659afaf293d1fc0bd951eb60e1168520aa712
Cristian Baciu <icristianbaciu@gmail.com>
2020-04-16 17:56:24 +0300

    Add full functionality

--- furnMax.Designer.cs
+++ furnMax.Designer.cs
@@ -28,17 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(furnMax));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nrSurse = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nrExemplare = new System.Windows.Forms.Label();
+            this.telefonFurnizor = new System.Windows.Forms.Label();
+            this.adresaFurnizor = new System.Windows.Forms.Label();
+            this.tipFurnizor = new System.Windows.Forms.Label();
+            this.denumireLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,58 +60,6 @@
             this.panel1.Size = new System.Drawing.Size(872, 125);
             this.panel1.TabIndex = 7;
             // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.pictureBox1);
-            this.panel3.Controls.Add(this.dataGridView1);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(872, 456);
-            this.panel3.TabIndex = 8;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AllowUserToResizeColumns = false;
-            this.dataGridView1.AllowUserToResizeRows = false;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 12F);
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(45)))), ((int)(((byte)(59)))));
-            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.EnableHeadersVisualStyles = false;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.RowTemplate.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(872, 456);
-            this.dataGridView1.TabIndex = 4;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(755, 26);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(77, 78);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            // 
             // label1
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
@@ -117,6 +72,159 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "Furnizorul cu nr. maximal de cărți";
             // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.nrSurse);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.nrExemplare);
+            this.panel3.Controls.Add(this.telefonFurnizor);
+            this.panel3.Controls.Add(this.adresaFurnizor);
+            this.panel3.Controls.Add(this.tipFurnizor);
+            this.panel3.Controls.Add(this.denumireLabel);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(872, 456);
+            this.panel3.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Orange;
+            this.label6.Location = new System.Drawing.Point(92, 368);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(290, 41);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Nr. de surse literare:";
+            // 
+            // nrSurse
+            // 
+            this.nrSurse.AutoSize = true;
+            this.nrSurse.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nrSurse.ForeColor = System.Drawing.Color.White;
+            this.nrSurse.Location = new System.Drawing.Point(388, 368);
+            this.nrSurse.Name = "nrSurse";
+            this.nrSurse.Size = new System.Drawing.Size(117, 41);
+            this.nrSurse.TabIndex = 15;
+            this.nrSurse.Text = "nrSurse";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Orange;
+            this.label2.Location = new System.Drawing.Point(92, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 41);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Nr. de exemplare:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Orange;
+            this.label3.Location = new System.Drawing.Point(92, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 41);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Telefon:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Orange;
+            this.label4.Location = new System.Drawing.Point(92, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 41);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Adresa:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Orange;
+            this.label5.Location = new System.Drawing.Point(92, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 41);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Tip:";
+            // 
+            // nrExemplare
+            // 
+            this.nrExemplare.AutoSize = true;
+            this.nrExemplare.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nrExemplare.ForeColor = System.Drawing.Color.White;
+            this.nrExemplare.Location = new System.Drawing.Point(355, 313);
+            this.nrExemplare.Name = "nrExemplare";
+            this.nrExemplare.Size = new System.Drawing.Size(182, 41);
+            this.nrExemplare.TabIndex = 10;
+            this.nrExemplare.Text = "nrExemplare";
+            // 
+            // telefonFurnizor
+            // 
+            this.telefonFurnizor.AutoSize = true;
+            this.telefonFurnizor.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.telefonFurnizor.ForeColor = System.Drawing.Color.White;
+            this.telefonFurnizor.Location = new System.Drawing.Point(217, 253);
+            this.telefonFurnizor.Name = "telefonFurnizor";
+            this.telefonFurnizor.Size = new System.Drawing.Size(219, 41);
+            this.telefonFurnizor.TabIndex = 9;
+            this.telefonFurnizor.Text = "telefonFurnizor";
+            // 
+            // adresaFurnizor
+            // 
+            this.adresaFurnizor.AutoSize = true;
+            this.adresaFurnizor.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adresaFurnizor.ForeColor = System.Drawing.Color.White;
+            this.adresaFurnizor.Location = new System.Drawing.Point(217, 192);
+            this.adresaFurnizor.Name = "adresaFurnizor";
+            this.adresaFurnizor.Size = new System.Drawing.Size(213, 41);
+            this.adresaFurnizor.TabIndex = 8;
+            this.adresaFurnizor.Text = "adresaFurnizor";
+            // 
+            // tipFurnizor
+            // 
+            this.tipFurnizor.AutoSize = true;
+            this.tipFurnizor.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipFurnizor.ForeColor = System.Drawing.Color.White;
+            this.tipFurnizor.Location = new System.Drawing.Point(166, 135);
+            this.tipFurnizor.Name = "tipFurnizor";
+            this.tipFurnizor.Size = new System.Drawing.Size(161, 41);
+            this.tipFurnizor.TabIndex = 7;
+            this.tipFurnizor.Text = "tipFurnizor";
+            // 
+            // denumireLabel
+            // 
+            this.denumireLabel.AutoSize = true;
+            this.denumireLabel.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.denumireLabel.ForeColor = System.Drawing.Color.White;
+            this.denumireLabel.Location = new System.Drawing.Point(318, 49);
+            this.denumireLabel.Name = "denumireLabel";
+            this.denumireLabel.Size = new System.Drawing.Size(343, 51);
+            this.denumireLabel.TabIndex = 6;
+            this.denumireLabel.Text = "denumireFurnizor";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(758, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(77, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
             // furnMax
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -128,10 +236,11 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "furnMax";
             this.Text = "furnMax";
+            this.Load += new System.EventHandler(this.furnMax_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -141,8 +250,18 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label nrExemplare;
+        private System.Windows.Forms.Label telefonFurnizor;
+        private System.Windows.Forms.Label adresaFurnizor;
+        private System.Windows.Forms.Label tipFurnizor;
+        private System.Windows.Forms.Label denumireLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label nrSurse;
     }
 }
--- furnMax.cs
+++ furnMax.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Bookstore
 {
@@ -15,6 +16,27 @@ namespace Bookstore
         public furnMax()
         {
             InitializeComponent();
+        }
+
+        private void furnMax_Load(object sender, EventArgs e)
+        {
+            // Database connection
+            SqlConnection sqlcon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\School\Anul III\Practica Anul III\bookstore-application\Bookstore.mdf;Integrated Security=True");
+
+            string query = "SELECT TOP(1) denumireFurnizor, tipFurnizor, adresaFurnizor, telefonFurnizor, SUM(nrExemplare) AS nr_exemplare, COUNT(idCarte) AS nr_surse FROM Furnizori ";
+            query += "INNER JOIN Carti ON Furnizori.idFurnizor = Carti.idFurnizor ";
+            query += "GROUP BY denumireFurnizor, tipFurnizor, adresaFurnizor, telefonFurnizor ORDER BY nr_surse DESC";
+            SqlDataAdapter sda = new SqlDataAdapter(query, sqlcon);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            // Show data from dataTable to labels
+            denumireLabel.Text = dt.Rows[0]["denumireFurnizor"].ToString();
+            tipFurnizor.Text = dt.Rows[0]["tipFurnizor"].ToString();
+            adresaFurnizor.Text = dt.Rows[0]["adresaFurnizor"].ToString();
+            telefonFurnizor.Text = dt.Rows[0]["telefonFurnizor"].ToString();
+            nrExemplare.Text = dt.Rows[0]["nr_exemplare"].ToString();
+            nrSurse.Text = dt.Rows[0]["nr_surse"].ToString();
         }
     }
 }
