7bb0011467d9945e8a98fd5b16568bba0f6aedab
Cristian Baciu <icristianbaciu@gmail.com>
2020-04-19 21:49:56 +0300

    Add language table

--- SQL Script/Create_Bookstore.sql
+++ SQL Script/Create_Bookstore.sql
@@ -29,6 +29,13 @@ create table Autori
     ,dataAutor nvarchar(100) -- Data nașterii autorului
     ,genAutor nvarchar(1) -- Genul (M sau F)
 )
+
+create table Limbi
+( 
+	idLimba int IDENTITY(1,1) primary key -- Cheie primară ce identifică limba
+	,numeLimba nvarchar(100) UNIQUE -- Limba
+)
+
 create table Carti
 ( 
 	idCarte int IDENTITY(1,1) primary key -- Cheie primară ce identifică cartea
@@ -36,10 +43,11 @@ create table Carti
 	,denumireCarte nvarchar(100) UNIQUE -- Denumirea cartii
     ,idAutor int foreign key (idAutor) references Autori -- Face legatura cu tabelul Autori
     ,anCarte nvarchar(100) -- Anul editarii cartii
-	,limbaCarte nvarchar(100) -- Limba cartii
+	,idLimba int foreign key (idLimba) references Limbi -- Face legatura cu tabelul L
     ,pretCarte int -- Pretul cartii
 	,nrExemplare int -- Numarul de exemplare
 )
+
 create table Users
 ( 
     idUser int IDENTITY(1,1) primary key -- Cheie primară ce identifică utilizatorul
@@ -61,17 +69,28 @@ INSERT INTO Autori (numeAutor, dataAutor, genAutor) VALUES
 	,('Miron Costin', '30.03.1633', 'M')
 	,('Veronica Micle', '22.04.1850', 'F')
 	,('Ion Pillat', '31.03.1891', 'M')
-	,(N'Luís de Camões', '01.12.1524', 'M')
+	,('Luís de Camões', '01.01.1524', 'M')
+	,(N'William Shakespeare', '26.04.1564', 'F')
+	,(N'Barbara Cartland', '09.07.1901', 'F')
+	,(N'Madame de La Fayette', '18.03.1634', 'F')
 GO
-INSERT INTO Carti (idFurnizor, denumireCarte, idAutor, anCarte, limbaCarte, pretCarte, nrExemplare) VALUES
-    (3, N'Amintiri din copilărie', 1, '1999', N'Română', 52, 0)
-    ,(1, N'Luceafărul', 2,'2000', N'Română', 86, 6)
-	,(4, N'Letopisețul Țării Moldovei', 3, 2000, N'Română', 138, 3)
-	,(5, N'Viața Lumii', 3, 2001, N'Română', 102, 14)
-	,(2, N'Plimbarea de mai în Iaşi', 4, 2001, N'Română', 83, 20)
-	,(2, 'Capra cu trei iezi', 1, 2001, N'Română', 52, 32)
-	,(1, N'La mormântul lui Aron Pumnul', 2, 2019, N'Română', 130, 10)
-	,(1, 'Lusiadele', 6, 2016, N'Portugheză', 240, 14)
+INSERT INTO Limbi (numeLimba) VALUES 
+	(N'Română')
+	,(N'Portugheză')
+	,(N'Franceză')
+	,(N'Engleză')
+	,(N'Rusă')
+	,(N'Italiană')
+GO
+INSERT INTO Carti (idFurnizor, denumireCarte, idAutor, anCarte, idLimba, pretCarte, nrExemplare) VALUES
+    (3, N'Amintiri din copilărie', 1, '1999', 1, 52, 0)
+    ,(1, N'Luceafărul', 2,'2000', 1, 86, 6)
+	,(4, N'Letopisețul Țării Moldovei', 3, 2000, 1, 138, 3)
+	,(5, N'Viața Lumii', 3, 2001, 1, 102, 14)
+	,(2, N'Plimbarea de mai în Iaşi', 4, 2001, 1, 83, 20)
+	,(2, 'Capra cu trei iezi', 1, 2001, 1, 52, 32)
+	,(1, N'La mormântul lui Aron Pumnul', 2, 2019, 1, 130, 10)
+	,(1, 'Lusiadele', 6, 2016, 2, 240, 14)
 GO
 INSERT INTO Users (username, password) VALUES 
     ('admin', 'admin')
